7e5623438f8316c3dc7cd63d0b816135a40f6f15
Rios06 <jenniriioss4@gmail.com>
2023-12-29 21:45:01 -0500

    Add frontend Country.jsx

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,28 +1,59 @@
-import { useQuery } from '@apollo/client';
-import React, { Fragment } from 'react';
+import React, {useState} from 'react';
+import { useQuery} from '@apollo/client';
+
 
 import GET_COUNTRIES from '../queries/countries.graphql';
+import '../Country.css';
 
 const Country = () => {
-
     const { data, error, loading } = useQuery(GET_COUNTRIES);
+const [searchTerm, setSearchTerm] = useState('');
+const [selectedContinent, setSelectedContinent] = useState('');
+
+    
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error... ${error.message}</p>
-    return (
-        <Fragment>
 
-            {data.countries.map((country) => (
-                <div key={country.name}>
-                <h2>{country.name}</h2>
-                <p>Code: {country.code}</p>
-                <p>Capital: {country.capital}</p>
-                <p>Continent: {country.continent ? country.continent.name : 'N/A'}</p>
-                <p>Currency: {country.currency}</p>
-                <p>Languages: {country.languages ? country.languages.map(lang => lang.name).join(', ') : 'N/A'}</p>
+
+    const filteredCountries = data.countries.filter((country) => {
+        return (
+          country.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+          (selectedContinent === '' || country.continent.name === selectedContinent)
+        );
+      });
+
+    return (
+        <div>
+        {/* Barra de búsqueda */}
+        <input
+          type="text"
+          placeholder="Buscar por país..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+  
+        {/* Menú de filtrado por continente */}
+        <select value={selectedContinent} onChange={(e) => setSelectedContinent(e.target.value)}>
+          <option value="">Todos los continentes</option>
+          {/* Agrega opciones para cada continente según tus datos */}
+        </select>
+  
+        {/* Lista de países */}
+      <div className="country-list">
+        {filteredCountries.map((country) => (
+          <div key={country.code} className="country-card">
+            <img src={country.flag} alt={`Bandera de ${country.name}`} />
+            <h2>{country.name}</h2>
+            <p>Continente: {country.continent.name}</p>
+            <p>Capital: {country.capital}</p>
+            <p>Moneda: {country.currency}</p>
+            <p>Idiomas: {country.languages.map((lang) => lang.name).join(', ')}</p>
+            {/* Agrega otros detalles según tus datos */}
             </div>
-            ))}
-        </Fragment>
+          ))}
+        </div>
+      </div>
     );
 };
 
