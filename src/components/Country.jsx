25d361d6b688903ee87a54e18361fe2cd8404074
Rios06 <jenniriioss4@gmail.com>
2023-12-30 00:15:55 -0500

    Add dba mongo

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -23,37 +23,58 @@ const [selectedContinent, setSelectedContinent] = useState('');
         );
       });
 
-    return (
+      const syncCountries = () => {
+        fetch('http://localhost:3000/api/countries/sync-countries', {
+          method: 'GET',
+        })
+          .then(response => {
+            console.log(response);
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log(data);
+            // Después de sincronizar, puedes recargar los datos
+          })
+          .catch(error => console.error('Error al sincronizar países:', error));
+      };
+
+      return (
         <div>
-        {/* Barra de búsqueda */}
-        <input
-          type="text"
-          placeholder="Buscar por país..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-  
-        {/* Menú de filtrado por continente */}
-        <select value={selectedContinent} onChange={(e) => setSelectedContinent(e.target.value)}>
-          <option value="">Todos los continentes</option>
-          {/* Agrega opciones para cada continente según tus datos */}
-        </select>
-  
-        {/* Lista de países */}
-      <div className="country-list">
-        {filteredCountries.map((country) => (
-          <div key={country.code} className="country-card">
-            <img src={country.flag} alt={`Bandera de ${country.name}`} />
-            <h2>{country.name}</h2>
-            <p>Continente: {country.continent.name}</p>
-            <p>Capital: {country.capital}</p>
-            <p>Moneda: {country.currency}</p>
-            <p>Idiomas: {country.languages.map((lang) => lang.name).join(', ')}</p>
-            {/* Agrega otros detalles según tus datos */}
-            </div>
-          ))}
+          {/* Barra de búsqueda */}
+          <input
+            type="text"
+            placeholder="Buscar por país..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+    
+          {/* Menú de filtrado por continente */}
+          <select value={selectedContinent} onChange={(e) => setSelectedContinent(e.target.value)}>
+            <option value="">Todos los continentes</option>
+            {/* Agrega opciones para cada continente según tus datos */}
+          </select>
+    
+          {/* Botón para sincronizar países */}
+          <button onClick={syncCountries}>Sincronizar Países</button>
+    
+          {/* Lista de países */}
+          <div className="country-list">
+            {filteredCountries.map((country) => (
+              <div key={country.code} className="country-card">
+                <img src={country.flag} alt={`Bandera de ${country.name}`} />
+                <h2>{country.name}</h2>
+                <p>Continente: {country.continent.name}</p>
+                <p>Capital: {country.capital}</p>
+                <p>Moneda: {country.currency}</p>
+                <p>Idiomas: {country.languages.map((lang) => lang.name).join(', ')}</p>
+                {/* Agrega otros detalles según tus datos */}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
     );
 };
 
